a370bfaf250f0035df43c96733c00c7bb6c23651
Leonardo Richard <77279633+BambamRR@users.noreply.github.com>
2023-01-23 04:02:42 -0300

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBI-iDKShLIyudIK_LnCTr_KlyPvl6aeg8",
+    authDomain: "expenses-controller-qwerty.firebaseapp.com",
+    projectId: "expenses-controller-qwerty",
+    storageBucket: "expenses-controller-qwerty.appspot.com",
+    messagingSenderId: "149583025055",
+    appId: "1:149583025055:web:b347ded6dc2928f41763aa",
+  };
+
+  // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
